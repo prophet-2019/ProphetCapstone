aa318df0da6b981cc1cf31031452eb2d3bd17d4d
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-26 11:51:40 -0500

    completed portfolio store. dispatching thunks from Company component is not connecting to our API back end calls

--- client/components/Company.js
+++ client/components/Company.js
@@ -1,19 +1,51 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getStockPriceToBuy, getStockPriceToSell} from '../store/portfolio'
 import HomePageChart from './HomePageChart'
 import CompanyData from './CompanyData'
 
 class Company extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {}
+
+  // arrow function should auto bind "this"
+  handleSubmitBuy = e => {
+    e.preventDefault()
+    this.props.buyStock()
+  }
+
+  handleSubmitSell = e => {
+    e.preventDefault()
+    this.props.buyStock()
+  }
   render() {
+    console.log('props are: \n\n\n', this.props)
     return (
       <div>
         <h3>Apple</h3>
         <HomePageChart />
         <CompanyData />
-        <button type="buy">Buy</button>
-        <button type="sell">Sell</button>
+        <button type="submit" onSubmit={this.handleSubmitBuy}>
+          Buy
+        </button>
+        <button type="submit" onSubmit={this.handleSubmitSell}>
+          Sell
+        </button>
       </div>
     )
   }
 }
 
-export default Company
+const mapDispatchToProps = dispatch => {
+  return {
+    buyStock: () => dispatch(getStockPriceToBuy()),
+    sellStock: () => dispatch(getStockPriceToSell())
+  }
+}
+
+const CompanyWithStore = connect(null, mapDispatchToProps)(Company)
+
+export default CompanyWithStore
