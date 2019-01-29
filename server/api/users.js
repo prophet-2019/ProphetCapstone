bef3546da7f56e4e64372b1f29613c433c579b64
Jay Bhagat <user.email>
2019-01-28 22:55:27 -0500

    buy, sell and get portfolio routes are working but need to be refactored to get user data

--- server/api/users.js
+++ server/api/users.js
@@ -26,6 +26,33 @@ router.get('/:userId/transactions', async (req, res, next) => {
   }
 })
 
+router.get('/:userId/portfolio', async (req, res, next) => {
+  try {
+    const usersTransactions = await Transaction.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    })
+    const getPortfolio = usersTransactions.reduce((accum, val) => {
+      let tickTick = val.dataValues.ticker
+      let shares = val.dataValues.transQuantity
+      let transType = val.dataValues.transactionType
+      if (transType === 'sell') {
+        shares *= -1
+      }
+      if (accum[tickTick] !== undefined) {
+        accum[tickTick] += shares
+      } else {
+        accum[tickTick] = shares
+      }
+      return accum
+    }, {})
+    res.send(getPortfolio)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/:userId/buy', async (req, res, next) => {
   try {
     const transactions = await Transaction.findAll({
@@ -40,22 +67,23 @@ router.post('/:userId/buy', async (req, res, next) => {
   }
 })
 
-router.get('/:userId/:portfolioId', async (req, res, next) => {
-  try {
-    const portfolios = await Portfolio.findById(req.params.portfolioId, {
-      include: [{model: Cash}, {model: Stock}]
-    })
-    res.status(202).send(portfolios)
-  } catch (err) {
-    next(err)
-  }
-})
+//Not using this anymore
+// router.get('/:userId/:portfolioId', async (req, res, next) => {
+//   try {
+//     const portfolios = await Portfolio.findById(req.params.portfolioId, {
+//       include: [{model: Cash}, {model: Stock}]
+//     })
+//     res.status(202).send(portfolios)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 router.put('/:userId/buy', async (req, res, next) => {
   // let stockTicker = req.body.symbol
   // let realTimeQuote = req.body.latestPrice
   // let quantity = req.body.quantity
-  let stockTicker = 'TSLA'
+  let stockTicker = 'GE'
   let realTimeQuote = 50
   let quantity = 2
   try {
@@ -67,123 +95,89 @@ router.put('/:userId/buy', async (req, res, next) => {
       // check to see if the user has enough cash
     })
     const user = await User.findById(req.params.userId)
-    console.log('User', user)
+    let cashValue = realTimeQuote * quantity
     if (user.dataValues.cash >= realTimeQuote * quantity) {
-      const buy = Transaction.create({
+      const buy = await Transaction.create({
         ticker: stockTicker,
         price: realTimeQuote,
         transQuantity: 100,
         transactionType: 'buy',
-        userId: req.params.userId,
-        stockId: 1
+        userId: req.params.userId
       })
       res.send(buy)
     } else {
       throw new Error('Not enough $$')
     }
-    // if yes, create the transaction.
-    // if not, throw error.
-    // then send updated portfolio
-
-    // let oldAmount = cash.dataValues.quantity
-    // const [numberOfRowsAffected, updatedCashInstance] = await Cash.update(
-    //   {
-    //     quantity: oldAmount - 1000
-    //   },
-    //   {
-    //     where: {
-    //       portfolioId: req.params.portfolioId
-    //     },
-    //     returning: true,
-    //     plain: true
-    //   }
-    // )
-
-    // const [stock] = await Stock.findAll({
-    //   where: {
-    //     portfolioId: req.params.portfolioId
-    //   }
-    // })
-    // let oldStockAmount = stock.dataValues.stockQuantity
-    // const [
-    //   numberOfStockRowsAffected,
-    //   updatedStockInstance
-    // ] = await Stock.update(
-    //   {
-    //     stockQuantity: oldStockAmount + 100,
-    //     name: stockTicker,
-    //     priceBoughtAt: realTimeQuote
-    //   },
-    //   {
-    //     where: {
-    //       portfolioId: req.params.portfolioId
-    //     },
-    //     returning: true,
-    //     plain: true
-    //   }
-    // )
-
-    // const portfolio = await Portfolio.findById(req.params.portfolioId, {
-    //   include: [{model: Cash}, {model: Stock}]
-    // })
-    // res.status(202).send(portfolio)
+    const oldCash = user.dataValues.cash
+    await User.update(
+      {
+        cash: oldCash - cashValue
+      },
+      {
+        where: {
+          id: req.params.userId
+        }
+      }
+    )
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/:userId/:portfolioId/sell', async (req, res, next) => {
-  let stockTicker = req.body.symbol
-  let realTimeQuote = req.body.latestPrice
+router.put('/:userId/sell', async (req, res, next) => {
+  //check if user owns that stock
+  let stockTicker = 'AAPL'
+  let realTimeQuote = 50
+  let shares = 2
+  let quantity = shares * realTimeQuote
+  let cashValue = realTimeQuote * quantity
   try {
-    const [cash] = await Cash.findAll({
+    const ownsIt = await Transaction.findAll({
       where: {
-        portfolioId: req.params.portfolioId
+        userId: req.params.userId,
+        ticker: stockTicker
       }
     })
-    let oldAmount = cash.dataValues.quantity
-    const [numberOfRowsAffected, updatedCashInstance] = await Cash.update(
-      {
-        quantity: oldAmount + 1000
-      },
-      {
-        where: {
-          portfolioId: req.params.portfolioId
+    if (!ownsIt) {
+      throw new Error("Sorry, you don't own this.")
+    }
+    //check all buys
+    //check all sells
+    const howMuch = ownsIt.reduce((accum, val) => {
+      let transType = val.dataValues.transactionType
+      let quant = val.dataValues.transQuantity
+      if (transType === 'sell') {
+        quant *= -1
+      }
+      accum += quant
+      return accum
+    }, 0)
+    //create sell transaction
+    if (howMuch >= quantity) {
+      const sellSellSell = await Transaction.create({
+        ticker: stockTicker,
+        price: realTimeQuote,
+        transQuantity: 100,
+        transactionType: 'sell',
+        userId: req.params.userId
+      })
+      const findUser = await User.findById(req.params.userId)
+      const oldCash = findUser.dataValues.cash
+      await User.update(
+        {
+          cash: oldCash + cashValue
         },
-        returning: true,
-        plain: true
-      }
-    )
-
-    const [stock] = await Stock.findAll({
-      where: {
-        portfolioId: req.params.portfolioId
-      }
-    })
-    let oldStockAmount = stock.dataValues.stockQuantity
-    const [
-      numberOfStockRowsAffected,
-      updatedStockInstance
-    ] = await Stock.update(
-      {
-        stockQuantity: oldStockAmount - 100,
-        name: stockTicker,
-        priceBoughtAt: realTimeQuote
-      },
-      {
-        where: {
-          portfolioId: req.params.portfolioId
-        },
-        returning: true,
-        plain: true
-      }
-    )
-
-    const portfolio = await Portfolio.findById(req.params.portfolioId, {
-      include: [{model: Cash}, {model: Stock}]
-    })
-    res.status(202).send(portfolio)
-  } catch (err) {
-    next(err)
+        {
+          where: {
+            id: req.params.userId
+          }
+        }
+      )
+      res.send(sellSellSell)
+    } else {
+      throw new Error("you don't own enough")
+    }
+  } catch (error) {
+    next(error.message)
   }
 })
