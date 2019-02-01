1b8bac619cc3bc62eab046b07457d3b19483dce2
Jay Bhagat <user.email>
2019-01-31 22:28:55 -0500

    assetAllocation chart pulling real-time data

--- client/components/AssetAllocation.js
+++ client/components/AssetAllocation.js
@@ -16,20 +16,19 @@ import {
   VerticalBarSeries,
   RadialChart
 } from 'react-vis'
-import {callbackify} from 'util'
 
 class AssetAllocation extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      portfolio: {},
-      portValues: [],
+      portfolio: [],
       intervalId: 0
     }
     this.interval = this.interval.bind(this)
   }
   async componentDidMount() {
     await this.interval()
+    this.setState({portfolio: this.props.portfolio})
   }
   async interval() {
     const callBack = func => {
@@ -44,51 +43,18 @@ class AssetAllocation extends Component {
   componentWillUnmount() {
     clearInterval(this.state.intervalId)
   }
-  // componentDidUpdate(prevProps, prevState) {
-  //   // Typical usage (don't forget to compare props):
-  //   //
-  //   //Thunk returns an object be sure to just grab price --> this is erroring out
-  //   //
-  //   // const portArr = Object.keys(this.state.portfolio);
-  //   // console.log("portArr", portArr)
-  //   // const portVal = portArr.map(async (val) => {
-  //   //   console.log("val", val)
-  //   //   await this.props.getStockPriceForAssetAllocation(val);
-  //   // })
-  //   // const isLoaded = prevState.portValues[0] === undefined
-  //   // console.log('Prev', isLoaded, 'State', this.state.portValues[0])
-  //   // const realTimePricePort = this.props.portfolioData.reduce(async (accum, val) => {
-  //   //   if (val.ticker !== 'MONEY') {
-  //   //     let total = await this.props.getStockPriceForAssetAllocation(val.ticker)
-  //   //     accum.push([
-  //   //       val.ticker,
-  //   //       val.quantity,
-  //   //       // this.props.getStockPriceForAssetAllocation(val.ticker),
-  //   //       total
-  //   //     ])
-  //   //   } else {
-  //   //     accum.push([val.ticker, val.quantity, val.quantity])
-  //   //   }
-  //   //   return accum
-  //   // }, [])
-  //   if (this.props.portfolioData !== prevProps.portfolioData) {
-  //     this.setState({
-  //       portfolio: this.props.portfolioData,
-  //       // portValues: realTimePricePort
-  //     })
-  //   }
-  // }
+  componentDidUpdate() {
+    if (this.state.portfolio !== this.props.portfolio) {
+      this.setState({portfolio: this.props.portfolio})
+    }
+  }
   render() {
     let myData
-    if (!this.state.portfolio) {
-      // const cashCash = this.state.cash || 0
-      // const portPort = this.state.stocks || 0
-      // // const cashCash = 2;
-      // // const portPort = 13;
-      // myData = Object.values(this.state.portfolio).reduce((accum, val) => {
-      //   accum.push({angle: val.quantity})
-      //   return accum
-      // }, [])
+    if (this.state.portfolio) {
+      myData = this.state.portfolio.reduce((accum, val) => {
+        accum.push({angle: val[1]})
+        return accum
+      }, [])
     } else {
       myData = [{angle: 0}, {angle: 0}, {angle: 100}]
     }
@@ -114,3 +80,38 @@ const mapDispatchToProps = dispatch => {
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(AssetAllocation)
 )
+
+// componentDidUpdate(prevProps, prevState) {
+//   // Typical usage (don't forget to compare props):
+//   //
+//   //Thunk returns an object be sure to just grab price --> this is erroring out
+//   //
+//   // const portArr = Object.keys(this.state.portfolio);
+//   // console.log("portArr", portArr)
+//   // const portVal = portArr.map(async (val) => {
+//   //   console.log("val", val)
+//   //   await this.props.getStockPriceForAssetAllocation(val);
+//   // })
+//   // const isLoaded = prevState.portValues[0] === undefined
+//   // console.log('Prev', isLoaded, 'State', this.state.portValues[0])
+//   // const realTimePricePort = this.props.portfolioData.reduce(async (accum, val) => {
+//   //   if (val.ticker !== 'MONEY') {
+//   //     let total = await this.props.getStockPriceForAssetAllocation(val.ticker)
+//   //     accum.push([
+//   //       val.ticker,
+//   //       val.quantity,
+//   //       // this.props.getStockPriceForAssetAllocation(val.ticker),
+//   //       total
+//   //     ])
+//   //   } else {
+//   //     accum.push([val.ticker, val.quantity, val.quantity])
+//   //   }
+//   //   return accum
+//   // }, [])
+//   if (this.props.portfolioData !== prevProps.portfolioData) {
+//     this.setState({
+//       portfolio: this.props.portfolioData,
+//       // portValues: realTimePricePort
+//     })
+//   }
+// }
