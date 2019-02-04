b6afbffb644d8b420fbb9783ac005f8bc50986b8
Ken Atienza <atienza.ken@gmail.com>
2019-02-04 12:00:15 -0500

    logout and logo inserted into header component, css flexbox misaligned

--- client/components/Template/Header.js
+++ client/components/Template/Header.js
@@ -1,30 +1,71 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
 import HeadlineComponent from './HeadlineComponent'
+import {Link} from 'react-router-dom'
+import {logout} from '../../store'
 
-export default class Header extends Component {
-  constructor(props) {
-    super(props)
+const Header = ({handleClick, isLoggedIn}) => (
+  <div className="header-child-container">
+    <nav>
+      {isLoggedIn ? (
+        <div className="isLoggedIn-container">
+          {/* The header will show these links after you log in */}
+          <div className="header-Logo">
+            <img
+              src="https://ih1.redbubble.net/image.697159210.3375/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u2.jpg"
+              as="prophet-logo"
+              height="50"
+              width="50"
+            />
+          </div>
+
+          <HeadlineComponent />
+
+          <div className="header-logout">
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+
+          <div className="header-profile">
+            <h5>Profile</h5>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {/* The header will show these links before you log in */}
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      )}
+    </nav>
+  </div>
+)
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user.id
   }
+}
 
-  render() {
-    return (
-      <div className="header-child-container">
-        <div className="header-Logo">
-          <h3>Logo</h3>
-        </div>
-
-        <HeadlineComponent />
-
-        <div className="header-logout">
-          <h5>Logout</h5>
-        </div>
-
-        <div className="header-profile">
-          <h5>Profile</h5>
-        </div>
-      </div>
-    )
+const mapDispatch = dispatch => {
+  return {
+    handleClick() {
+      dispatch(logout())
+    }
   }
+}
+
+export default connect(mapState, mapDispatch)(Header)
+
+/**
+ * PROP TYPES
+ */
+Header.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
 }
