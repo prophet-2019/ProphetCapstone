36c06450255023169c56904d4e954d01a5ea8050
Ken Atienza <atienza.ken@gmail.com>
2019-02-05 02:14:17 -0500

    default particle.js rendering, need dollar sign to render

--- client/app.js
+++ client/app.js
@@ -3,7 +3,11 @@ import Routes from './routes'
 import {getPriceFromAPI} from './store/ticker'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
+import particle from './particle'
+import 'particles.js/particles'
+import Particles from 'react-particles-js'
 
+const particlesJS = window.particlesJS
 class App extends Component {
   componentDidMount() {
     this.props.getPriceFromAPI()
@@ -11,6 +15,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Particles className="particles-js" params={particle} />
+        {particlesJS.load(
+          'particles-js',
+          '../public/particles.json',
+          function() {
+            console.log('loaded')
+          }
+        )}
         <Routes />
       </div>
     )
