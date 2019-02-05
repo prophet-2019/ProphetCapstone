c29ef297590af76755cd6b794cd3a345f81b71ab
Jay Bhagat <user.email>
2019-02-05 12:00:21 -0500

    added conditional setInterval to cut number of times that getPortfolioData thunk is running -- restart CPU and check on heroku

--- client/components/CompanyFinancials.js
+++ client/components/CompanyFinancials.js
@@ -12,7 +12,17 @@ class CompanyData extends Component {
   componentDidMount() {}
 
   render() {
-    this.props.getPortfolioData(this.props.ticker)
+    const currentTicker = this.props.ticker
+    let count = 0
+    if (currentTicker === this.props.ticker && count === 0) {
+      setInterval(() => this.props.getPortfolioData(this.props.ticker), 10000)
+      count++
+    } else if (currentTicker !== this.props.ticker && count !== 0) {
+      console.log('currentTicker not matching was Matching hit', count)
+      this.props.getPortfolioData(currentTicker)
+    } else {
+      setInterval(() => this.props.getPortfolioData(this.props.ticker), 10000)
+    }
     const labelsOfFinancialReport = Object.keys(this.props.stats)
     const valuesFromFinancialReport = Object.values(this.props.stats)
     const arrToMapThroughInComponent = [
