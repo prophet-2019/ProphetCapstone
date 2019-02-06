29721836c55cda81c2df325a697a192af45dc698
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-02-06 16:52:37 -0500

    phase 1

--- client/components/Template/CompanyDetails.js
+++ client/components/Template/CompanyDetails.js
@@ -1,18 +1,62 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {getPortfolioData} from '../store/companyDetailsTable'
 import {withRouter} from 'react-router'
-import Ticker from '../Ticker'
 
-export default class CompanyDetails extends Component {
+class CompanyDetails extends Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.ticker !== prevProps.ticker) {
+      this.props.getPortfolioData(this.props.ticker)
+    }
+  }
+
   render() {
+    const labelsOfFinancialReport = Object.keys(this.props.stats)
+    const valuesFromFinancialReport = Object.values(this.props.stats)
+    const arrToMapThroughInComponent = [
+      [labelsOfFinancialReport],
+      [valuesFromFinancialReport]
+    ]
     return (
-      <div className="details">
-        <h5>Company Details</h5>
+      <div className="companyDetails-container">
+        <h5>Company Prices</h5>
+        {this.props.ticker ? (
+          <div className="financialList">
+            <Table striped>
+              <Table.Body>
+                {labelsOfFinancialReport.map((val, idx) => {
+                  return (
+                    <Table.Row key={idx}>
+                      <Table.Cell>{labelsOfFinancialReport[idx]}</Table.Cell>
+                      <Table.Cell>{valuesFromFinancialReport[idx]}</Table.Cell>
+                    </Table.Row>
+                  )
+                })}
+              </Table.Body>
+            </Table>
+          </div>
+        ) : null}
       </div>
     )
   }
 }
+const mapStateToProps = state => {
+  return {
+    stats: state.companyDetailsTable.stats,
+    ticker: state.chart.ticker
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getPortfolioData: ticker => dispatch(getPortfolioData(ticker))
+  }
+}
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CompanyDetails)
+)
