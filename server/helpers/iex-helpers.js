f682c1337c7b6a25a17699c1e8c948fc1f9938ed
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-10 12:40:51 -0400

    move key to see if that's causing the error

--- server/helpers/iex-helpers.js
+++ server/helpers/iex-helpers.js
@@ -1,5 +1,5 @@
-const IEX_API_KEY = process.env.IEX_API_KEY;
-const IEX_PREFIX = 'https://cloud.iexapis.com/v1';
-const IEX_SUFFIX = `?token=${IEX_API_KEY}`;
+const IEX_API_KEY = 'pk_c011eff3cb66451498a2bc8866945f1d'
+const IEX_PREFIX = 'https://cloud.iexapis.com/v1'
+const IEX_SUFFIX = `?token=${IEX_API_KEY}`
 
-export default {IEX_PREFIX, IEX_SUFFIX};
+export default {IEX_PREFIX, IEX_SUFFIX}
