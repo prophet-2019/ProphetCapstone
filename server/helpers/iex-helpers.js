ecf8c73b34a01ceafd1b19b0e874804790707427
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-10 14:07:58 -0400

    Procfile

--- server/helpers/iex-helpers.js
+++ server/helpers/iex-helpers.js
@@ -1,4 +1,4 @@
-const IEX_API_KEY = 'pk_c011eff3cb66451498a2bc8866945f1d'
+const IEX_API_KEY = process.env.IEX_API_KEY
 const IEX_PREFIX = 'https://cloud.iexapis.com/v1'
 const IEX_SUFFIX = `?token=${IEX_API_KEY}`
 
